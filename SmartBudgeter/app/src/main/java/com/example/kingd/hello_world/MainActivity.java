52d80df4d0d6cadfd63c1d0efab6ade159da83d9
Kevin Tain <ktain@ucsd.edu>
2016-02-10 08:57:36 -0800

    Changed show more button IDs. Added futurePayDays and moreEvents activities.

--- SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/MainActivity.java
+++ SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/MainActivity.java
@@ -29,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void buttonOnClick(View v) {
+        Button button = (Button) v;
+        
 
     }
 
