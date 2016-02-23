ea3e9f3c1a861691c31cda228f461dd6c7d90996
Vincent Lim <vincentlim@vincents-mbp.dynamic.ucsd.edu>
2016-02-22 22:18:22 -0800

    Added a Navigation Drawer

--- SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/MainActivity.java
+++ SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.kingd.hello_world;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -19,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
     DBFetch dbFetch;
 
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +38,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Code for Navigation Bar
+        mDrawerList = (ListView)findViewById(R.id.navList);mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+            }
+        };
+
 
         dbFetch = new DBFetch();
         dbFetch.readFromFile();
-        if(!dbFetch.isReadEmpty()){
+/*        if(!dbFetch.isReadEmpty()){
             dbFetch.rePopulateFromRead();
             dbFetch.sortHistoryByDate();
             dbFetch.sortUnpaidByDate();
@@ -37,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         else {
             dbFetch.setBalance(0.00);
         }
-
+*/
 
         // Update Next Payment fields
         TextView dateField = (TextView) findViewById(R.id.amountField);
@@ -61,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
         notesField2.setText("");
 
         //Calendar Part
-        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+/*        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
                                              @Override
@@ -90,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
                                              }
                                          }
 
-        );
+        ); */
 
 
 
@@ -120,4 +154,47 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Calendar", "Add Payment/Expenses", "Edit Payment/Expenses",
+                "Delete Payment/Expenses", "Home Page" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 }
