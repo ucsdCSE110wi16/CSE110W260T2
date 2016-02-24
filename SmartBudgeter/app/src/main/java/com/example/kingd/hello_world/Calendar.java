7374120dfc6e57a4b314df20558c5655a9e0cbaa
Vincent Lim <vincentlim@vincents-mbp.dynamic.ucsd.edu>
2016-02-24 12:36:25 -0800

    update UI to colorScheme

--- SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/Calendar.java
+++ SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/Calendar.java
@@ -13,6 +13,39 @@ public class Calendar extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        //Calendar Part
+/*        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                                             @Override
+                                             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                                                 int index = 0;
+                                                 Date tempD = new Date(year, month, dayOfMonth);
+                                                 String selectedDate = Integer.toString(year) + '/' + Integer.toString(month) + '/' + Integer.toString(dayOfMonth);
+                                                 DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+                                                 Date date = new Date();
+                                                 if (date.compareTo(tempD) == -1) {
+                                                     TextView calendarCategory = (TextView) findViewById(R.id.CategoryShow);
+                                                     TextView calendarAmount = (TextView) findViewById(R.id.AmountShow);
+                                                     while (index < dbFetch.getFuture().size() - 1) {
+                                                         if (dbFetch.getFuture().get(index).getPaymentDate().equals(selectedDate)) {
+                                                             //if(calendarCategory.getText() != null){
+                                                             calendarCategory.setText(dbFetch.getFuture().get(index).getCategories());
+                                                             calendarAmount.setText(Double.toString(dbFetch.getFuture().get(index).getTransactionAmt()));
+                                                             //}
+
+                                                         } else {
+                                                             calendarCategory.setText("N/A");
+                                                             calendarAmount.setText("N/A");
+                                                         }
+                                                     }
+                                                 }
+                                             }
+                                         }
+
+        ); */
     }
+
 
 }
