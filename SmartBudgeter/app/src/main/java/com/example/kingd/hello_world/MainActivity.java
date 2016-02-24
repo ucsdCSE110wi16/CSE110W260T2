469016c53a043fd062200678b9888502fa96195f
Kevin Tain <ktain@ucsd.edu>
2016-02-24 11:16:30 -0800

    Added Calendar class

--- SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/MainActivity.java
+++ SmartBudgeter/app/src/main/java/com/example/kingd/hello_world/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.kingd.hello_world;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
